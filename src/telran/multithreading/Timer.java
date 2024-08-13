7357d64152fdc7f8af99b4abe804dcd695407eb7
bbl <bbl@host118.MAIN.MFVSEGEI.RU>
2024-08-14 01:06:19 +0300

    HW #45 commit #1

--- src/telran/multithreading/Timer.java
+++ src/telran/multithreading/Timer.java
@@ -1,22 +1,85 @@
 package telran.multithreading;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Timer extends Thread {
 //TODO displaying time in a given format and a given resolution
 	//example displaying each second, or each 5 seconds, etc.
-	public Timer() {
-		setDaemon(true);
-	}
-	public void run() {
-		while(true) {
-			System.out.println(LocalTime.now());
-			try {
-				sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	long pauseTime=1000;
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+	private final Object lock = new Object();
+	private boolean paused = false;
+
+	public void setTimeFormat(String timeFormat)
+	{
+		try
+		{
+			this.formatter = DateTimeFormatter.ofPattern(timeFormat);
+		} 
+		catch (IllegalArgumentException e)
+		{
 		}
 	}
+	
+	public void setPauseTime(long pauseTime)
+	{
+		this.pauseTime = pauseTime;
+	}
+	
+	public Timer() 
+	{
+		setDaemon(true);
+	}
+	
+	public void run()
+	{
+		while(true) 
+		{
+			synchronized (lock)
+			{
+				while (paused) 
+				{
+					try 
+					{
+						lock.wait();
+					}
+					catch (InterruptedException e) 
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			System.out.print("\r"+" ".repeat(20)+LocalTime.now().format(formatter));
+			
+			try
+			{
+				Thread.sleep(pauseTime); 
+			} 
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}		
+	}
+	
+	public void pauseThread() 
+	{
+		synchronized (lock) 
+		{
+			paused = true;
+		}
+	}
+
+	public void resumeThread()
+	{
+		synchronized (lock) 
+		{
+			paused = false;
+			lock.notify();
+		}
+	}
+	
 }
--- src/telran/multithreading/TimerControllerAppl.java
+++ src/telran/multithreading/TimerControllerAppl.java
@@ -1,11 +1,55 @@
 package telran.multithreading;
 
+import java.util.Scanner;
+
 public class TimerControllerAppl {
 
 	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Command words for timer: start/stop/exit/time <time>/format <format>/exit");
+		Thread.sleep(2000);
+		
 		Timer timer = new Timer();
 		timer.start();
-		Thread.sleep(10000);
+
+		
+		Scanner scanner = new Scanner(System.in); 
+		String input="";
+		while (input.compareToIgnoreCase("exit")!=0)
+		{
+			input = scanner.nextLine();
+			String strCommand="";
+			String strPar="";
+			int index= input.indexOf(' ');
+			if(index!=-1) 
+			{
+				strCommand=input.substring(0, index);
+				strPar=input.substring(index).trim();					
+			}
+			else strCommand=input;
+				
+			
+			switch(strCommand)
+			{
+				case "start": timer.resumeThread(); break;
+				case "stop":  timer.pauseThread(); break;
+				case "time": 
+					{
+						try
+						{
+							timer.setPauseTime(Long.parseLong(strPar)); 
+							
+						} 
+						catch (NumberFormatException e)
+						{
+						
+						}
+						break;
+					}
+				case "format": timer.setTimeFormat(strPar); break;				
+			}
+		}			 
+		scanner.close(); 
+
 		//TODO
 		//figure out a solution allowing timer stop
 		//as example in 5 seconds you stop the timer
